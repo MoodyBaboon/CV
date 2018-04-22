90e8ac027eb30c6bf55d3e19c063ecbc2e0bdd2b
MoodyBaboon <lar_k@ukr.net>
2018-04-22 21:28:45 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -149,7 +149,7 @@ window.onload = function () {
             for(var i = 0; i < eveElements.length; i++) {
                 eveElements[i].remove();
             } 
-            birdsOff();
+            cancelAnimationFrame(birdsId);
         }, 4000); 
         setTimeout(function(){               
             for(var i = 0; i < nightElements.length; i++) {
